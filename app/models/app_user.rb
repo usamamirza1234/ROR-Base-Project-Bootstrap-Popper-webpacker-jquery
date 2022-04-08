2cb4932865dfe9766d08fa7a050433241920d6bf
Usama Mirza <usamak.mirza@gmail.com>
2022-04-08 17:41:39 +0500

    devise custom parameters added

--- app/models/app_user.rb
+++ app/models/app_user.rb
@@ -9,11 +9,30 @@ class AppUser < ApplicationRecord
          :confirmable,
          :lockable,
          :timeoutable,
-         :trackable
+         :trackable,
+         authentication_keys: [:login]
 
+
+  validates :username, presence: true ,uniqueness: true
+  attr_accessor :login
+
+  # method for virtual attribute
+  # self used for current object
+  def login
+    @login || self.username || self.email
+  end
   private
 
   def after_confirmation
     WelcomeMailer.send_greeting_notification(self).deliver_now
+  end
+
+  def self.find_for_database_authentication(warden_condition)
+    conditions = warden_condition.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", {value: login.downcase}]).first
+    elsif conditions.has_key? (:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
   end
 end
